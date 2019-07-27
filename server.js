31ebdfb561303e2912d579dc25f1b06de96c323d
VerySneaky77 <carlquin77@gmail.com>
2019-07-27 16:26:11 -0700

    Added and edited files for running from server, edited package dependencies to run app server-side

---
+++ server.js
@@ -0,0 +1,32 @@
+require("dotenv").config(); //TK
+// Express initialization
+const express = require("express");
+const mongoose = require("mongoose");
+const routes = require("./routes");
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/public"));
+}
+
+app.use(routes);
+
+// Add routes, both API and view
+// Index call TK
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname + "/client/public/index.html"));
+});
+
+// Connect to the Mongo DB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/restaurant");
+
+// Start the API server
+app.listen(PORT, function() {
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+});
