4ca1f95f50040380af687f122032fe52d78d17ed
VerySneaky77 <carlquin77@gmail.com>
2019-08-14 15:15:27 -0700

    Added github pages hosting

--- client/src/components/VisitorWindow/NewVisitorWindow.js
+++ client/src/components/VisitorWindow/NewVisitorWindow.js
@@ -1,26 +1,31 @@
-import React from "react";
+import React, { Component } from "react";
 import { Card, Container, Button, Form, FormGroup, Label, Input, FormText } from "reactstrap";
 import "./style.css";
 
-export default function NewVisitorWindow() {
-    return (
-        <Card className="window-card">
-            <Container>
-                <Form>
-                    <h2>New Visitor</h2>
-                    <FormGroup>
-                        <Label for="name">Name for Orders</Label>
-                        <Input type="textarea" name="name" id="name" placeholder="Enter Your Name" />
-                        <FormText color="muted">Must be one name. White space will be ignored.</FormText>
-                    </FormGroup>
-                    <FormGroup>
-                        <Label for="phone">Phone Number</Label>
-                        <Input type="textarea" name="phone" id="phone" placeholder="Enter Your Phone Number" />
-                        <FormText color="muted">Only enter numeric characters.</FormText>
-                    </FormGroup>
-                    <Button>Submit</Button>
-                </Form>
-            </Container>
-        </Card>
-    );
+class NewVisitorWindow extends Component {
+
+    render() {
+        return (
+            <Card className="window-card">
+                <Container>
+                    <Form>
+                        <h2>New Registration</h2>
+                        <FormGroup>
+                            <Label for="name">Name for Orders</Label>
+                            <Input type="textarea" name="name" id="name" placeholder="Enter Your Name" />
+                            <FormText color="muted">Must be one name. White space will be ignored.</FormText>
+                        </FormGroup>
+                        <FormGroup>
+                            <Label for="phone">Phone Number</Label>
+                            <Input type="textarea" name="phone" id="phone" placeholder="Enter Your Phone Number" />
+                            <FormText color="muted">Only enter numeric characters.</FormText>
+                        </FormGroup>
+                        <Button>Register</Button>
+                    </Form>
+                </Container>
+            </Card>
+        );
+    }
 }
+
+export default NewVisitorWindow;
