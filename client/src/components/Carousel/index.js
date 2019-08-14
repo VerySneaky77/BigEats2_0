23a28870b5b944e077f91703b396d01b0775fc8a
VerySneaky77 <carlquin77@gmail.com>
2019-08-13 18:38:05 -0700

    Changed home carousel style

--- client/src/components/Carousel/index.js
+++ client/src/components/Carousel/index.js
@@ -5,30 +5,22 @@ import './style.css';
 const items = [
   {
     src: 'http://www.nisiadigital.com.br/wp-content/uploads/2013/06/Pizza-pizza-30424279-1024-768.jpg',
-    altText: 'Pizza',
-    caption: 'Have a slice!',
-    header: 'Delicious Pizza'
+    altText: 'Pizza'
   },
   {
     src: 'http://myfoodvoice.me/wp-content/uploads/2011/03/Burgers.jpg',
-    altText: 'Burgers',
-    caption: '1/2 lbs Burgers',
-    header: 'Big Burgers'
+    altText: 'Burgers'
   },
   {
     src: 'http://i.ytimg.com/vi/0y9fSgUwNuo/maxresdefault.jpg',
-    altText: 'Chicken',
-    caption: 'Fried Chicken',
-    header: 'Cluck, cluck'
+    altText: 'Chicken'
   },
   {
     src: 'https://rapidbrandsinc.com/wp-content/uploads/2017/05/Screenshot.png',
-    altText: 'Hot Dog',
-    caption: 'Hot Dogs',
-    header: 'Dog is in the bun'
+    altText: 'Hot Dog'
   }
 ];
 
-const CarouselHome = () => <UncontrolledCarousel items={items} controls={false}/>;
+const CarouselHome = () => <UncontrolledCarousel indicators={false} items={items} controls={false}/>;
 
 export default CarouselHome;
