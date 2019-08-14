6476e4a8d3c8ab5f0582f93395b1d1227f00c390
VerySneaky77 <carlquin77@gmail.com>
2019-08-13 17:03:26 -0700

    Adjust front-end home

--- client/src/pages/Home/VisitorHome.js
+++ client/src/pages/Home/VisitorHome.js
@@ -3,17 +3,18 @@ import { Jumbotron, Media, Container, Card, Row, Col } from "reactstrap";
 import Carousel from "../../components/Carousel";
 import "./style.css";
 
-const pizza = require('../../images/pizza.jpg');
-const burger = require('../../images/burger.jpg');
-const chicken = require('../../images/chicken.jpeg');
-const hotdog = require('../../images/hot dog.jpg');
+const pictures = [
+    new URL('https://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/Parker_Training_Academy_Dutch_Barn%2C_Red_Hook%2C_NY.jpg/1200px-Parker_Training_Academy_Dutch_Barn%2C_Red_Hook%2C_NY.jpg'),
+    new URL('https://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/Parker_Training_Academy_Dutch_Barn%2C_Red_Hook%2C_NY.jpg/1200px-Parker_Training_Academy_Dutch_Barn%2C_Red_Hook%2C_NY.jpg'),
+    new URL('https://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/Parker_Training_Academy_Dutch_Barn%2C_Red_Hook%2C_NY.jpg/1200px-Parker_Training_Academy_Dutch_Barn%2C_Red_Hook%2C_NY.jpg')
+]
 
 class VisitorHome extends Component {
     render() {
         return (
             <>
                 <div className="carousel-container">
-                    <Carousel className="carousel"/>
+                    <Carousel className="carousel" />
                 </div>
                 <Jumbotron fluid className="tranparent" id="ptag">
                     <Container>
@@ -25,20 +26,15 @@ class VisitorHome extends Component {
                 <Card fluid className="tranparentbox">
                     <Container>
                         <div className="image-box">
-                            <Media src={burger} className="image" fluid />
+                            <Media src={pictures[0]} className="image" fluid />
                         </div>
-                        <p className="discriptiion">
-                            Try one of our
-                            delicious specialty
-                            pizzas and find your
-                            new favorite!
-                        </p>
+                        <p className="discriptiion">Consequat do Lorem amet eu. Consequat est eu eu labore eiusmod aliquip pariatur nulla enim in consequat labore. Eu aute laboris amet in. Laborum reprehenderit aliquip aliquip anim esse incididunt nulla fugiat non cupidatat irure excepteur aliqua. Ipsum dolor eu excepteur voluptate enim nulla ex aute minim labore est incididunt cupidatat cillum. Et et aliquip dolor quis fugiat fugiat ad amet consectetur Lorem magna do cupidatat. Dolor consequat ullamco id ex nulla qui cupidatat id adipisicing deserunt consectetur deserunt excepteur cupidatat.</p>
                     </Container>
                 </Card>
                 <Card fluid className="tranparentbox">
                     <Container>
                         <div className="image-box">
-                            <Media src={chicken} className="image" fluid />
+                            <Media src={pictures[1]} className="image" fluid />
                         </div>
                         <p className="discriptiion">
                             Start your summer
@@ -50,14 +46,14 @@ class VisitorHome extends Component {
                 <Card fluid className="tranparentbox">
                     <Container>
                         <div className="image-box">
-                            <Media src={hotdog} className="image" fluid />
+                            <Media src={pictures[2]} className="image" fluid />
                         </div>
                         <p className="discriptiion">
                             Classic and custome made
                             hotdogs
                         </p>
                     </Container>
-                </Card> 
+                </Card>
             </>
         )
     }
